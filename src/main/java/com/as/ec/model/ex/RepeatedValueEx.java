148ecefd4b05fbfb318681320b1dab768111e3a1
Rodrigo Montiel <rmcrodrigo1@gmail.com>
2020-09-09 13:00:17 -0700

    Almost final changes

--- src/main/java/com/as/ec/model/ex/RepeatedValueEx.java
+++
@@ -1,9 +0,0 @@
-package com.as.ec.model.ex;
-
-public class RepeatedValueEx extends RuntimeException {
-	private static final long serialVersionUID = -7256769266411615334L;
-
-	public RepeatedValueEx(String error) {
-		super(error);
-	}
-}
--- src/main/java/com/as/ec/model/ex/UserNotFoundException.java
+++
@@ -1,9 +0,0 @@
-package com.as.ec.model.ex;
-
-public class UserNotFoundException extends RuntimeException {
-	private static final long serialVersionUID = -6437157094962318456L;
-
-	public UserNotFoundException(String error) {
-		super(error);
-	}
-}
