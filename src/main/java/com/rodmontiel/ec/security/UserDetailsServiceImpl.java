c906e8ea41538881e101c804cf3ba2b5f3a1f6f8
Rodrigo Montiel <ext.rodrigo.montiel@pricetravel.com>
2021-07-03 14:15:39 +0000

    feat: Replacing generic exception by UsernameNotFoundException

--- src/main/java/com/rodmontiel/ec/security/UserDetailsServiceImpl.java
+++ src/main/java/com/rodmontiel/ec/security/UserDetailsServiceImpl.java
@@ -5,6 +5,7 @@ import java.util.ArrayList;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Component;
 
 import com.rodmontiel.ec.ex.GenericExceptionHandler;
@@ -21,7 +22,7 @@ public class UserDetailsServiceImpl implements UserDetailsService {
 	public UserDetails loadUserByUsername(String email) throws GenericExceptionHandler {
 
 		User user = uRepository.getUserByEmail(email)
-				.orElseThrow(() -> new GenericExceptionHandler(113));
+				.orElseThrow(() -> new UsernameNotFoundException("Invalid credendials"));
 
 		return new org.springframework.security.core.userdetails.User(user.getEmail(), user.getPassword(),
 				user.isEnabled(), true, true, true, new ArrayList<>());
